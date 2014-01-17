e31fc043db07263c712141fb88cbca4a5ed0c589
Cody Creager <pi.rubiks@gmail.com>
2014-01-17 11:40:41 -0500

    Added simple error messages.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,25 @@
  *      Author: cody
  */
 
-int main(int argc, char** argv)
+#include <stdio.h>
+
+int main(int argc, char* argv[])
 {
+	FILE* fp;
+	
+	if(argc != 2)
+	{
+		printf("Invalid Arguments!\n");
+		return 1;
+	}
+	
+	fp = fopen(argv[1], "r");
+	if(!fp)
+	{
+		printf("Invalid File!\n");
+		return 2;
+	}
+	fclose(fp);
+	
 	return 0;
 }
