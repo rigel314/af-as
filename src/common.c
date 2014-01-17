65b8a24e598df60ecd734909c15cad9c8029cc1c
Cody Creager <pi.rubiks@gmail.com>
2014-01-17 16:38:51 -0500

    Added useful functions and defined ASCIIshiftBit.

--- src/common.c
+++ src/common.c
@@ -5,4 +5,34 @@
  *      Author: cody
  */
 
+#include <stdlib.h>
+#include <string.h>
 
+/**
+ * int strchrCount(char* s, char c)
+ * 	s is a string.
+ * 	c is a character to look for.
+ * returns number of occurrences of c in s.
+ */
+int strchrCount(char* s, char c)
+{
+	int i;
+	for (i = 0; s[i]; (s[i] == c) ? (void) i++ : (void) s++); // always increment s or i.  Casts to void to avoid warnings.
+	return i;
+}
+
+char* strnchr(char* s, char c, int len)
+{
+	for(int i=0; i<len; i++)
+	{
+		if(s[i] == c)
+			return &s[i];
+	}
+	return NULL;
+}
+
+void strShiftLeft(char* s)
+{
+	for(int i = 0; i < strlen(s); i++)
+		s[i] = s[i+1];
+}
--- src/common.h
+++ src/common.h
@@ -8,6 +8,11 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+// ASCII shiftBit for ignoring case.
+#define ASCIIshiftBit 0x20
 
+int strchrCount(char* s, char c);
+char* strnchr(char* s, char c, int len);
+void strShiftLeft(char* s);
 
 #endif /* COMMON_H_ */
