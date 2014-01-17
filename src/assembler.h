7e56b120784a1f85573f2a1812c7039686919013
Cody Creager <pi.rubiks@gmail.com>
2014-01-17 16:36:43 -0500

    Added structs to describe lines of assembly code.

--- src/assembler.h
+++ src/assembler.h
@@ -8,6 +8,46 @@
 #ifndef ASSEMBLER_H_
 #define ASSEMBLER_H_
 
+enum instArgType { argType_Unused, argType_Immediate, argType_Register };
 
+enum lineType { lineType_None, lineType_Instruction, lineType_Label, lineType_Byte, lineType_Error };
+
+struct label
+{
+	char* name;
+};
+
+struct instruction
+{
+	int arg1;
+	int arg2;
+	char instruction[5];
+	char type1;
+	char type2;
+	char width;
+};
+
+struct byte
+{
+	char val;
+};
+
+union line
+{
+	struct label lbl;
+	struct instruction inst;
+	struct byte byte;
+};
+
+struct lineinfo
+{
+	union line line;
+	int address;
+	int lineNum;
+	char type;
+};
+
+int structify(char* source, struct lineinfo** lines);
+void freeLineinfos(struct lineinfo* lines, int len);
 
 #endif /* ASSEMBLER_H_ */
